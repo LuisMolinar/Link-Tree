fc48a370ff01d5cd7de41fca93f166c8db424118
Luis Molina <luisemr14molina@gmail.com>
2022-08-18 21:23:26 -0400

    tipyng

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Engineer","Developer", "Blogger", "Designer", "Freelancer","TheAppWizard"],
+        strings: ["Developer", "Blogger", "Designer", "Freelancer","TheAppWizard"],
         typeSpeed: 100,
         backSpeed: 60,
         cursorChar: ' |',
